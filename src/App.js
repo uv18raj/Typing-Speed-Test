fbc18a9a32a1515ae21ae060ad16ceda4207039c
uv18raj <utsavraj9131@gmail.com>
2024-09-05 17:03:30 +0530

    website created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SpeedTypingGame from "./components/SpeedTypingGame";
+
+function App() {
+  return (
+    <div className="App">
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default App;
